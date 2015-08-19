c7a2e56612fee93b174c948f48141560ce5d6ec3
senlis <senlis@users.noreply.github.com>
2015-08-19 12:19:24 +0800

    add defalult actor

---
+++ senlis-akka/src/main/java/senlis/akka/app/akka_test/DefaultActor.java
@@ -0,0 +1,13 @@
+package senlis.akka.app.akka_test;
+
+import akka.actor.UntypedActor;
+
+public class DefaultActor extends UntypedActor {
+
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
